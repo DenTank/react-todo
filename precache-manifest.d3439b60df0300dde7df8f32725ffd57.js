2050f4d3894a54deed7d7f0e69e86b66981c9e42
tank <tank@test.com>
2020-09-28 21:02:38 +0300

    Updates

---
+++ precache-manifest.d3439b60df0300dde7df8f32725ffd57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a06c5b9292361ce867cf94ef9ccbc1",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "d247a91db0c9334de74c",
+    "url": "/react-todo/static/css/main.fea4efe8.chunk.css"
+  },
+  {
+    "revision": "70ad550193770d8eaae7",
+    "url": "/react-todo/static/js/2.7bd92741.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.7bd92741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d247a91db0c9334de74c",
+    "url": "/react-todo/static/js/main.fce16420.chunk.js"
+  },
+  {
+    "revision": "b81e150b308975bc2389",
+    "url": "/react-todo/static/js/runtime-main.89ae8f88.js"
+  }
+]);
